bcee6811b9253a9d37f22c6337568a1fd95d93da
YessMtz <105814773+YessMtz@users.noreply.github.com>
2023-05-03 09:49:02 -0700

    Yess

--- Unidad 2 Librerias/UserControl1.Designer.cs
+++ Unidad 2 Librerias/UserControl1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Unidad_2_Librerias
 {
-    partial class UserControl1
+    partial class UserCtrl
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserCtrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "UserCtrl";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.Load += new System.EventHandler(this.UserCtrl_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Unidad 2 Librerias/UserControl1.cs
+++ Unidad 2 Librerias/UserControl1.cs
@@ -7,14 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Unidad_2_Librerias
 {
-    public partial class UserControl1: UserControl
+    public partial class UserCtrl: UserControl
     {
-        public UserControl1()
+        //variables
+        private string directorio = "";
+        private int dimension;
+        private int separacion;
+        private int borde = 7;
+        
+        //coleccion de imagenes
+        //declaracion de la lista tipo imagen 
+        private List   <Imagen> imagenes = new List<Imagen>();
+
+        //PROPIEDADES
+        //directorio de las imagenes
+        public string Directorio {
+            get { return directorio; }
+            set { directorio = value;
+                UpdateControl();
+            }
+        }
+
+        //dimensiones de las imagenes
+        public int Dimension {
+            get { return dimension; }
+            set { UpdateControl(); 
+            }
+        }
+
+        //SEPARACION
+        public int Sepracion { 
+            get { return separacion; }
+            set { separacion = value;
+                UpdateControl();
+            }
+        }
+
+        public UserCtrl()
         {
             InitializeComponent();
+        }
+
+        private void getImagenes() {
+
+            if (directorio.Length !=0) 
+            { 
+                imagenes.Clear();
+                DirectoryInfo dire = new DirectoryInfo(directorio);
+                foreach (FileInfo fi in dire.GetFiles("*.jpg"))
+                {
+                    imagenes.Add(new Imagen(Bitmap.FromFile(File.), File.Fullname));
+                }
+            }
+        }
+
+        //clase que se usara en la lista, conetiene las caracteristicas del objeto
+        internal class Imagen
+        {
+            //creacion de objeto de la clase
+
+            public Imagen img{get; set; }
+            public string FullName{ get; set; }
+
+            public Imagen(Imagen img, string fileName )
+            {
+                this.img = img;
+                FileName = fileName;
+            }
+
+
+        }
+        private void UserCtrl_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
